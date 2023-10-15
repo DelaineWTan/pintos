1482c8cf05020c66a7dc2f2949f6dddf73e67001
Delaine Tan <delainewtan@gmail.com>
2023-10-15 12:53:30 -0700

    Re-implement timer_sleep and related functions to avoid busy waiting.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    int64_t wakeup_time;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
@@ -137,5 +138,5 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
-
+void wake_up_sleeping_threads(int64_t current_ticks);
 #endif /* threads/thread.h */
